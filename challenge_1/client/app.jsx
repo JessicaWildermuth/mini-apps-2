8aec041359f13bd2cf3bf916595a1851dc7e2845
JessicaWildermuth <jwildermuth536@gmail.com>
2020-11-04 14:07:49 -0700

    Add get request with full text search to json.db

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,17 +1,50 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      search: '',
+      results: null,
     };
+    this.updateSearch = this.updateSearch.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  updateSearch(e) {
+    e.preventDefault();
+    this.setState({
+      search: e.target.value,
+    });
+  }
+
+  search(e) {
+    e.preventDefault();
+    const { search } = this.state;
+    console.log(search);
+    axios({
+      method: 'get',
+      url: `http://localhost:3000/events?q=${search}`,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+      });
   }
 
   render() {
+    const { search } = this.state;
     return (
       <div>
-        REACT HAS COMPILED AND LOADED
+        <form onSubmit={this.search}>
+          <label htlmfor="search"> Search For Historical Event </label>
+          <input type="text" value={search} onChange={this.updateSearch} />
+          <input type="submit" onSubmit={this.search} />
+        </form>
       </div>
     );
   }
